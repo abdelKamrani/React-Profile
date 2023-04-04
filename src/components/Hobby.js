d8cb05b28bad677b5228e2d016a5c2d9dfb8eb76
Coursera <aide.abdel@gmail.com>
2023-04-04 12:50:26 -0400

    onClicktoHover

--- src/components/Hobby.js
+++ src/components/Hobby.js
@@ -1,28 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-function Hobby({value,name,img,quote,text}) {
-
-    // const back = ["back","hobby-flipped"]
-    const [back,setBack] = useState(["back","hobby-flipped"])
-    function flipHobby(){
-        setBack(["delete-back","hobby"])
-    }
+function Hobby({ value, name, img, quote, text }) {
+  // const back = ["back","hobby-flipped"]
+  const [back, setBack] = useState(["back", "hobby-flipped"]);
+  function flipHobby() {
+    setBack(["delete-back", "hobby"]);
+  }
   return (
-<  >
-    <div className={back[1]}>
-        <div className='hobby-title'>{name}</div>
-        <div className='hobby-image-container'>
-            <img className='hobby-image' src={img} alt='my hobby'/>
+    <>
+      <div className={back[1]}>
+        <div className="hobby-title">{name}</div>
+        <div className="hobby-image-container">
+          <img className="hobby-image" src={img} alt="my hobby" />
         </div>
-        
-        <div className='hobby-quote'> {quote}</div>
-        <div className='hobby-text'> {text}</div>
-    </div>
-    <div className={back[0]} onClick={flipHobby}>
+
+        <div className="hobby-quote"> {quote}</div>
+        <div className="hobby-text"> {text}</div>
+      </div>
+      <div className={back[0]} onMouseEnter={flipHobby}>
         {value}
-    </div>
+      </div>
     </>
-)
+  );
 }
 
-export default Hobby
+export default Hobby;
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,33 +1,31 @@
-import React from 'react'
-import Menu from '../components/Menu'
-import {useState,useEffect} from 'react'
-import ReposContainer from '../components/ReposContainer';
+import React from "react";
+import Menu from "../components/Menu";
+import { useState, useEffect } from "react";
+import ReposContainer from "../components/ReposContainer";
 
 function Portfolio() {
-
-  const [repo,setRepo] = useState(null);
-  const token = "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";
+  const [repo, setRepo] = useState(null);
+  const token = "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";
   const username = "aide-abdel";
 
-  const getRepo = async(token,username)=>{
+  const getRepo = async (token, username) => {
     const apiUrl = `https://api.github.com/users/${username}/repos`;
-    const headers = {'Authorization': `Bearer ${token}`};
-    const response = await fetch(apiUrl,{headers});
+    const headers = { Authorization: `Bearer ${token}` };
+    const response = await fetch(apiUrl, { headers });
 
     const data = await response.json();
     console.log(data);
     setRepo(data);
-  }
-  useEffect(()=>{
-    getRepo(token,username);
-  },[]);
+  };
+  useEffect(() => {
+    getRepo(token, username);
+  }, []);
   return (
-    <div className='portfolio'>
-    <Menu/>
-    <ReposContainer repos={repo}/>
+    <div className="portfolio">
+      <Menu />
+      <ReposContainer repos={repo} />
     </div>
-    
-  )
+  );
 }
 
-export default Portfolio
+export default Portfolio;
